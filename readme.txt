928d298634069b2a34c700271c6b324d45519260
Meezo <mozeiny@gmail.com>
2018-09-13 13:52:16 -0700

    personal credentials are wiped from the script

---
+++ readme.txt
@@ -0,0 +1,68 @@
+# Lazy Academy 
+Here is a short script I wrote for automatic sign-in using Python, selenium, and an STP library. IMPORTANT: If any TAs are reading this right now pls ignore this is just another assessment practice repo. 
+
+
+## Getting Started
+
+Please read the directions carefully. I’ve tried my best to streamline these things for you. However- and this is important- if after reading this if you still have any questions please please do not find me. If I have to stop whatever I’m doing at the moment to help you I will be annoyed. Justin Han would be happy to answer your questions though. 
+
+## Prerequisites
+
+Python and related programs,  Chromewebdriver, and about an hour. 
+
+## Installing
+
+Install python, pip, and virtual env. In the terminal type the following
+
+```
+sudo brew install python
+sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py”
+sudo pip install --upgrade virtualenv
+
+```
+
+Get Chromewebdriver
+
+* Download and install chrome webdriver for your computer. For simplicity, I recommend installing it in the same file as where you are storing the python web driver
+
+## Configuring to your system
+
+Open up they python script, and enter you github email and github
+username and password. Now you have a working script.
+
+## Configuring Automatic Text updates and Password
+
+This part can be a bit tricky, since you have to configure your email settings to allow remote logins. If you're using gmail just use this guide and it should work. 
+
+https://support.google.com/accounts/answer/6009563 
+
+Also FYI you can send a text through email.
+* if you are using verizion it's YOURPHONENUMBER@vtext.com
+* if you are using tmobile it's  YOURPHONENUMBER@@tmomail.net
+* if you are using at&t it's "YOURPHONENUMBER@txt.att.net"
+
+## Scheduling the task
+
+For automatic running of the script I would recommend utilizing Cron. It comes preloaded with all linux and macs.  In the crontab type: 
+
+'''
+ Crontab -e
+ Select "nano" option
+'''
+
+Enter the following strings in crontab. Don't forget to edit the absolute path!
+ 
+ 50 8   *   *   1-5    bash -c "python absolutepath/webdriver.py
+ 0  13  *   *   1-5    bash -c "python absolutepath/webdriver.py
+ 55 15  *   *   1-5    bash -c "python absolutepath/webdriver.py
+
+Enjoy!
+
+## Author
+
+* TheLastSultan
+* Long live Telegraph Hill
+
+
+
---
+++ webdriver.py
@@ -0,0 +1,101 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import NoSuchElementException
+import time
+import smtplib
+
+
+# Download and install chromedriver, then link the path into this variable
+# CHROME_DRIVER_ADDRESS
+chromedriver = "/home/mobro/Desktop/pythonwebdriver/chromedriver"
+
+# change this for automatic texts
+PHONE_NUMBER_OR_EMAIL=" YOURPHONENUMBER@txt.att.net"
+    # use a phone number or email above
+    # if you are using verizion it's YOURPHONENUMBER@vtext.com
+    # if you are using tmobile it's  YOURPHONENUMBER@@tmomail.net
+YOUR_GITHUB_USERNAME = "TheLastSultan"
+YOUR_GITHUB_PASS = "allah199"
+
+
+
+
+gmail_user = "YOUREMAIL@email.com"
+gmail_password = "YOURPASSWORD@email.com"
+
+def send_alert_email(subject, body, to=[]):
+    """
+    subject: string
+    body: string
+    return boolean
+    """
+
+    from_mail = gmail_user
+    # to = ['shan.z@leantaas.com']
+
+    email_text = """From: %s
+To: %s
+Subject: %s
+
+%s""" % (from_mail, ', '.join(to), subject, body)
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(from_mail, to, email_text)
+    server.close()
+
+def send_successful_text():
+    send_alert_email("Login attempt Successful!'", "We be alright out here", to=[PHONE_NUMBER_OR_EMAIL] )
+
+def send_unsuccessful_text():
+    send_alert_email("Login attempt FAILED", "Don't forget to log in yourself", to=[PHONE_NUMBER_OR_EMAIL])
+
+driver = webdriver.Chrome(chromedriver)
+
+# Github login
+
+# Don't change the sleep times. If it runs too fast it will set off site alarms
+def login():
+    time.sleep(1)
+    WebDriverWait(driver,10)
+
+    # Navigate to website
+    driver.get("https://progress.appacademy.io/me/sign_in")
+
+    # Find sign in
+    driver.find_element_by_partial_link_text('Sign in with').click()
+    time.sleep(1)
+
+    # Enter your username and password
+    driver.find_element_by_id('login_field').send_keys("TheLastSultan")
+    driver.find_element_by_id('password').send_keys("allah199")
+    driver.find_element_by_name('commit').click()
+    time.sleep(1)
+
+    # You are logged in, let's click the form
+    driver.find_element_by_class_name('student-check-in-form').click()
+
+
+
+try:
+    login()
+except NoSuchElementException:
+    send_unsuccessful_text()
+else:
+    send_successful_text()
+    
+
+
+
+# There is occasionally an authentication page
+
+# Headless options
+# need to download chrome cnry
+# from selenium.webdriver.chrome.options import Options
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# from selenium.webdriver.common.keys import Keys
+# driver = webdriver.Chrome(chrome_options = chrome_options)
